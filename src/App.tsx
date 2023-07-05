3b3c7c15d227035ac4002a980ac8c5f03b7ed067
joaovictordesouzaduarte <victorduarte.ufrj@gmail.com.br>
2023-07-05 20:44:49 -0300

    ordering

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,25 @@
 import { Routes, Route } from "react-router-dom";
+import PaginaBaseAdmin from "./paginas/Administracao/PaginaBaseAdmin";
+import AdministracaoRestaurantes from "./paginas/Administracao/Restaurantes/AdministracaoRestaurantes";
+import FormularioRestaurante from "./paginas/Administracao/Restaurantes/FormularioRestaurante";
 import Home from "./paginas/Home";
 import VitrineRestaurantes from "./paginas/VitrineRestaurantes";
-import { AdministracaoRestaurantes } from "./paginas/Administracao/Restaurantes/AdministracaoRestaurantes";
+import AdministracaoPratos from "./paginas/Administracao/Pratos/AdministracaoPratos";
+import FormularioPrato from "./paginas/Administracao/Pratos/FormularioPrato";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/restaurantes" element={<VitrineRestaurantes />} />
-      <Route
-        path="/admin/restaurantes"
-        element={<AdministracaoRestaurantes />}
-      />
+
+      <Route path="/admin" element={<PaginaBaseAdmin />}>
+        <Route path="restaurantes" element={<AdministracaoRestaurantes />} />
+        <Route path="restaurantes/novo" element={<FormularioRestaurante />} />
+        <Route path="restaurantes/:id" element={<FormularioRestaurante />} />
+        <Route path="pratos" element={<AdministracaoPratos />} />
+        <Route path="pratos/novo" element={<FormularioPrato />} />
+      </Route>
     </Routes>
   );
 }
--- src/componentes/ListaRestaurantes/index.tsx
+++ src/componentes/ListaRestaurantes/index.tsx
@@ -1,41 +1,106 @@
+import axios from "axios";
 import { useEffect, useState } from "react";
+import { IPaginacao } from "../../interfaces/IPaginacao";
 import IRestaurante from "../../interfaces/IRestaurante";
 import style from "./ListaRestaurantes.module.scss";
 import Restaurante from "./Restaurante";
-import axios from "axios";
-import { IPaginacao } from "../../interfaces/IPaginacao";
-const ListaRestaurantes = () => {
+import {
+  Box,
+  Button,
+  Input,
+  InputLabel,
+  Select,
+  Typography,
+  MenuItem,
+  SelectChangeEvent,
+} from "@mui/material";
+
+interface ListaRestaurantesProps {}
+const ListaRestaurantes = (props: ListaRestaurantesProps) => {
   const [restaurantes, setRestaurantes] = useState<IRestaurante[]>([]);
   const [proximaPagina, setProximaPagina] = useState("");
-  useEffect(() => {
+  const [paginaAnterior, setPaginaAnterior] = useState("");
+  const [busca, setBusca] = useState("");
+  const [ordenacao, setOrdenacao] = useState("");
+  // const classes = useStyles(props);
+
+  const carregarDados = (url: string) => {
     axios
-      .get<IPaginacao<IRestaurante>>(
-        "http://localhost:8000/api/v1/restaurantes/"
-      )
+      .get<IPaginacao<IRestaurante>>(url)
       .then((resposta) => {
         setRestaurantes(resposta.data.results);
         setProximaPagina(resposta.data.next);
+        setPaginaAnterior(resposta.data.previous);
       })
-      .catch((erro) => console.log(erro));
-  }, []);
-  const verMais = (): void => {
-    axios
-      .get<IPaginacao<IRestaurante>>(proximaPagina)
-      .then((resposta) => {
-        setRestaurantes([...restaurantes, ...resposta.data.results]);
-        setProximaPagina(resposta.data.next);
-      })
-      .catch((erro) => console.log(erro));
+      .catch((erro) => {
+        console.log(erro);
+      });
   };
+  const buscar = (evento: React.FormEvent<HTMLFormElement>) => {
+    evento.preventDefault();
+    carregarDados(
+      `http://localhost:8000/api/v1/restaurantes/?search=${busca}&ordering=${ordenacao}`
+    );
+  };
+  useEffect(() => {
+    // obter restaurantes
+    carregarDados("http://localhost:8000/api/v1/restaurantes/");
+  }, []);
+
   return (
     <section className={style.ListaRestaurantes}>
-      <h1>
-        Os restaurantes mais <em>bacanas</em>!
-      </h1>
+      <Typography variant="h5" component="h2">
+        Os restaurantes mais <em>bacanas</em> !
+      </Typography>
+      <form onSubmit={buscar}>
+        <Input
+          type="text"
+          value={busca}
+          onChange={(evento) => setBusca(evento.target.value)}
+          placeholder="Pesquise aqui o seu restaurante"
+          sx={{ minWidth: 300 }}
+        />
+        <InputLabel id="select-ordenacao"></InputLabel>
+        <Select
+          value={ordenacao}
+          sx={{ minWidth: 300, ml: 5 }}
+          labelId="select-ordenacao"
+          onChange={(evento) => setOrdenacao(evento.target.value)}
+        >
+          <MenuItem value="">Padrão</MenuItem>
+          <MenuItem value="id">Por ID</MenuItem>
+          <MenuItem value="nome">Por nome</MenuItem>
+        </Select>
+
+        <Button
+          sx={{ ml: 3 }}
+          size="large"
+          type="submit"
+          variant="contained"
+          color="primary"
+        >
+          Buscar
+        </Button>
+      </form>
       {restaurantes?.map((item) => (
         <Restaurante restaurante={item} key={item.id} />
       ))}
-      {proximaPagina && <button onClick={verMais}>Ver mais</button>}
+      {
+        <button
+          onClick={() => carregarDados(paginaAnterior)}
+          disabled={!paginaAnterior}
+        >
+          Página Anterior
+        </button>
+      }
+      {
+        <button
+          onClick={() => carregarDados(proximaPagina)}
+          disabled={!proximaPagina}
+        >
+          Próxima página
+        </button>
+      }
     </section>
   );
 };
